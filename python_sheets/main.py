de23250914332ee577966d0c35ad28d1362a9f74
rcsang <sangmath@outlook.com>
2025-01-09 21:45:39 +0100

    commit

--- python_sheets/main.py
+++ python_sheets/main.py
@@ -16,7 +16,7 @@ async def favicon():
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Next.js frontend URL
+    allow_origins=["*"],  # Next.js frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
